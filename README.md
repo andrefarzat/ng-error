3c57918f8cda9a65aa21068fcddfa77b124138b0
Andre Farzat <andrefarzat@gmail.com>
2014-05-02 19:57:09 -0400

    Adding directive in github

--- README.md
+++ README.md
@@ -2,3 +2,30 @@ ng-error
 ========
 
 Angular ngError directive
+
+
+### Installing
+
+Install via `bower`
+
+```sh
+bower install ng-error --save
+```
+
+Add a `<script>` to your html
+
+```html
+<script src="bower_components/ng-error/ng-error.js"></script>
+```
+
+### Usage
+
+```js
+// add 'ngError' as dependency to your module
+var yourModule = angular.module("yourModule", ['ngError']);
+```
+
+You can add the `ng-error` directive in `<img>` tags
+```html
+<img src="beautifulImage.jpg" ng-error="someFunction()" alt="" />
+````
---
+++ ng-error.js
@@ -0,0 +1,27 @@
+(function(angular){
+
+    angular.module('ngError', ['ng'])
+    
+    .directive('ngError', ['$parse', function($parse){
+
+        return {
+            restrict: 'A',
+            require: 'ngSrc',
+            compile: function($element, attr) {
+                var fn = $parse(attr['ngError']);
+
+                return function(scope, element, attr) {
+                    element.on('error', function(event) {
+                        scope.$apply(function() {
+                            fn(scope, {$event:event});
+                        });
+                    });
+                };
+
+            }
+        };
+
+    }]);
+
+    
+})(angular);
